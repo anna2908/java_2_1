3be64cc0f34c164a630f9ea8f32b533a9f435499
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-02 19:04:00 +0300

    Add project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int TicketPrice = 110;
+        int BonusPrice = 20;
+
+        System.out.println("Стоимость билета - " + TicketPrice + " руб.");
+        System.out.println("Начисленно бонусов - " +(TicketPrice/BonusPrice));
+    }
+}
