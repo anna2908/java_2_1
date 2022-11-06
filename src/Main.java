9ba9fbfe3115251245f57593d5c06d707738463e
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-06 16:40:28 +0300

    Change names

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int TicketPrice = 110;
-        int BonusPrice = 20;
+        int ticketPrice = 110;
+        int bonusPrice = 20;
 
-        System.out.println("Стоимость билета - " + TicketPrice + " руб.");
-        System.out.println("Начисленно бонусов - " +(TicketPrice/BonusPrice));
+        System.out.println("Стоимость билета - " + ticketPrice + " руб.");
+        System.out.println("Начисленно бонусов - " +(ticketPrice/bonusPrice));
     }
 }
