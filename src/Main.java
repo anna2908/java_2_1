3e94417cf990e05a670e4ec244a8d4e5de23b77b
Anna Beloshkurnik <squirrelpochta2@mail.ru>
2022-11-09 18:30:52 +0300

    Format

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         int ticketPrice = 110;
         int bonusPrice = 20;
 
         System.out.println("Стоимость билета - " + ticketPrice + " руб.");
-        System.out.println("Начисленно бонусов - " +(ticketPrice/bonusPrice));
+        System.out.println("Начисленно бонусов - " + (ticketPrice / bonusPrice));
     }
 }
